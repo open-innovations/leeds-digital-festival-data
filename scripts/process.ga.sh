b7888ed87ab4f2168917d0295cd9a44d03d49023
Giles Dring <giles@dringtech.com>
2021-11-09 09:33:14 +0000

    Adding Google Analytics data

---
+++ scripts/process.ga.sh
@@ -0,0 +1,1 @@
+deno run --unstable --allow-read --allow-write scripts/process.ga.ts "working/Google Analytics - Users & Page Views.csv"
---
+++ scripts/process.ga.ts
@@ -0,0 +1,13 @@
+import { readCSV, writeCSV } from 'https://deno.land/x/flat/mod.ts';
+import { setupData } from './util.ts';
+const { makeDataPath } = await setupData('2021-09');
+
+const filename = Deno.args[0];
+
+const websiteData = (await readCSV(filename)).map((d: any) => ({
+  date: new Date(d['Day Index']).toISOString().split('T')[0],
+  users: parseInt(d['Users']),
+  pageviews: parseInt(d['Pageviews']),
+}));
+
+await writeCSV(makeDataPath('website.csv'), websiteData)
