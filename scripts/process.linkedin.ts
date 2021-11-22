3ca2925f58392fee3ebbf8eb88d56de312eccd7f
Giles Dring <giles@dringtech.com>
2021-11-22 15:04:52 +0000

    Add linkedin update stats

--- scripts/process.linkedin.ts
+++ scripts/process.linkedin.ts
@@ -3,28 +3,67 @@ import { setupData } from './util.ts';
 
 const { makeDataPath } = await setupData('2021-09');
 
-const [visitorsFile, rest] = Deno.args;
-
-const visitorWorkbook = await readXLSX(visitorsFile);
-
-const sheetData = visitorWorkbook.Sheets[visitorWorkbook.SheetNames[0]];
-const data = await xlsx.utils.sheet_to_json(sheetData);
-
-function parseData(date: string) {
+function parseDate(date: string) {
   const [m, d, y] = date.split('/');
   return `${y}-${m}-${d}`;
 }
 
+const [visitorsFile, updatesFile, rest] = Deno.args;
+
+const updatesWorkbook = await readXLSX(updatesFile);
+const updateData = xlsx.utils.sheet_to_json(
+  updatesWorkbook.Sheets[updatesWorkbook.SheetNames[0]],
+  {
+    range: 1,
+  }
+);
+
+const visitorWorkbook = await readXLSX(visitorsFile);
+const visitorData = xlsx.utils.sheet_to_json(
+  visitorWorkbook.Sheets[visitorWorkbook.SheetNames[0]]
+);
+
 function processVisitors(raw: any) {
-  const { Date: date, 'Total page views (total)': total_page_views_total, 'Total unique visitors (total)': total_unique_visitors_total } = raw;
+  const {
+    Date: date,
+    'Total page views (total)': total_page_views_total,
+    'Total unique visitors (total)': total_unique_visitors_total,
+  } = raw;
   return {
-    date: parseData(date),
+    date: parseDate(date),
     total_page_views_total,
     total_unique_visitors_total,
   };
 }
 
+function processUpdates(raw: any) {
+  const {
+    Date: date,
+    'Impressions (total)': impressions_total,
+    'Clicks (total)': clicks_total,
+    'Reactions (total)': reactions_total,
+    'Comments (total)': comments_total,
+    'Shares (total)': shares_total,
+    'Engagement rate (total)': engagement_rate_total,
+  } = raw;
+
+  return {
+    date: parseDate(date),
+    impressions_total,
+    clicks_total,
+    reactions_total,
+    comments_total,
+    shares_total,
+    engagement_rate_total: (engagement_rate_total as Number).toFixed(3),
+  };
+}
+
 await writeCSV(
   makeDataPath('linkedin-visitors.csv'),
-  data.map(processVisitors)
+  visitorData.map(processVisitors)
+);
+
+await writeCSV(
+  makeDataPath('linkedin-updates.csv'),
+  updateData.map(processUpdates)
 );
