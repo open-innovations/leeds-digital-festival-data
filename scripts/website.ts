2828b03b189cfad8bb82acb1408f310c4221519c
Giles Dring <giles@dringtech.com>
2021-12-08 16:59:28 +0000

    Fix formatting of website stats

--- scripts/website.ts
+++ scripts/website.ts
@@ -5,8 +5,9 @@ const { makeDataPath } = await setupData('services');
 const filename = Deno.args[0];
 
 const parseDate = (date: string) => {
-  const [ m, d, y ] = date.split('/')
-  return `${y}-${m}-${d}`;
+  const [ m, d, y ] = date.split('/').map((x) => parseInt(x, 10));
+  const dtObj = new Date(y + (y < 2000 ? 2000 : 0), m-1, d, 12, 0, 0);
+  return dtObj.toISOString().split('T').shift();
 };
 
 const websiteData = (await readCSV(filename)).map((d: any) => ({
