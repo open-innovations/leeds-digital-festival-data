4fb6eae87928ac8ab501d133b9a9456fc553f724
Giles Dring <giles@dringtech.com>
2022-08-20 10:37:02 +0100

    Create new home page + layout

--- _config.ts
+++ _config.ts
@@ -1,5 +1,6 @@
 import lume from "lume/mod.ts";
 import base_path from "lume/plugins/base_path.ts";
+import inline from "lume/plugins/inline.ts";
 
 const site = lume({
   location: new URL("https://open-innovations.github.io/leeds-digital-festival-data/"),
@@ -7,6 +8,7 @@ const site = lume({
 });
 
 site.use(base_path());
+site.use(inline());
 
 // Copy HTML files
 site.copy(['.html']);
@@ -16,5 +18,9 @@ site.copy('data');
 
 // Prevent gh-pages from using jekyll
 site.copy('.nojekyll')
+
+// Provision images
+site.remoteFile('/assets/images/logo.svg', 'https://leedsdigitalfestival.org/wp-content/themes/leeds-digital-festival/assets/images/logo.svg');
+site.copy('/assets/images')
 
 export default site;
