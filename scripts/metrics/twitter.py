c34c6c785baf535486e042d029630a3a56f3c7e7
Giles Dring <giles@dringtech.com>
2022-09-22 10:14:53 +0000

    Tidying up metrics

--- scripts/metrics/twitter.py
+++ scripts/metrics/twitter.py
@@ -7,25 +7,42 @@ WORKING_DIR = os.path.join('working', 'twitter')
 OUTPUT_DIR = os.path.join('data', 'twitter')
 RAW_FILE = os.path.join(OUTPUT_DIR, 'twitter.csv')
 
-files = glob.glob(os.path.join(WORKING_DIR, '*.csv'))
 
 def read_existing():
     data = None
     return data
 
+
 def save_existing(data):
     os.makedirs(OUTPUT_DIR, exist_ok=True)
     data.to_csv(RAW_FILE)
 
+
 def load_file(filename):
-    data = pd.read_csv(filename, parse_dates=['Date'], index_col=['Date'], na_values=['-'])
-    data.index = data.index.set_names([x.lower().replace('\s+', '_') for x in data.index.names])
+    data = pd.read_csv(filename, parse_dates=['Date'], index_col=[
+                       'Date'], na_values=['-'])
+    data.index = data.index.set_names(
+        [x.lower().replace('\s+', '_') for x in data.index.names])
     data.columns = data.columns.str.lower().str.replace('\s+', '_', regex=True)
     return data
 
-data = read_existing()
 
-if (data == None):
-    data = pd.concat([load_file(file) for file in files]).sort_values(by=['date'])
+def update_raw_data():
 
-save_existing(data)
+    data = read_existing()
+
+    files = glob.glob(os.path.join(WORKING_DIR, '*.csv'))
+
+    if (len(files) == 0):
+        print('Nothing to do! No new files found in "{}".'.format(WORKING_DIR))
+        return
+
+    if (data == None):
+        data = pd.concat([load_file(file)
+                         for file in files]).sort_values(by=['date'])
+
+    save_existing(data)
+
+
+if __name__ == '__main__':
+    update_raw_data()
