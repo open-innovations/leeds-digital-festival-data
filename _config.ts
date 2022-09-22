ed3d0a577893f21689aaf1cbf5213d04b719966a
Giles Dring <giles@dringtech.com>
2022-09-22 10:53:49 +0000

    Create basic metrics page

--- _config.ts
+++ _config.ts
@@ -2,6 +2,7 @@ import lume from "lume/mod.ts";
 import base_path from "lume/plugins/base_path.ts";
 import inline from "lume/plugins/inline.ts";
 import date from "lume/plugins/date.ts";
+import csvLoader from 'oi-lume-utils/loaders/csv-loader.ts';
 
 const site = lume({
   location: new URL("https://open-innovations.github.io/leeds-digital-festival-data/"),
@@ -27,5 +28,8 @@ site.copy('.nojekyll')
 // Provision images
 site.remoteFile('/assets/images/logo.svg', 'https://leedsdigitalfestival.org/wp-content/themes/leeds-digital-festival/assets/images/logo.svg');
 site.copy('/assets/images')
+
+// Register data loaders
+site.loadData([".csv"], csvLoader);
 
 export default site;
