71b1218f5e446b0f2498acc70b85b53b5c58b19f
Daniel Boam <daniel.boam@open-innovations.org>
2022-11-16 12:20:29 +0000

    Handle free form text for region info

--- scripts/metrics/questionaire/region_count.py
+++ scripts/metrics/questionaire/region_count.py
@@ -6,7 +6,11 @@ URL = "https://docs.google.com/spreadsheets/d/1rqdwoyVO1TOHjWfllQYm5JUK9Q8-W233h
 
 data = pd.read_csv(URL)
 
+data = data[data['Full Name'] != 'GILES DRING TEST DATA']
 total_reporting = data.shape[0]
+
+data = data[data['Please identify the regions within the UK that your event(s) welcomed attendees from:'].str.contains('|'.join(REGIONS))]
+total_reporting_region = data.shape[0]
 
 region_count = (data.rename(columns={'Please identify the regions within the UK that your event(s) welcomed attendees from:' : 'Region'})             
                     .filter(['Region'])
@@ -18,8 +22,16 @@ region_count = (data.rename(columns={'Please identify the regions within the UK 
 #Turning tuples in to Strings
 region_count = {region[0] : region_count[region] for region in region_count.keys() if region[0] in REGIONS}
 
-max_region = max(region_count.values())
+#MANUAL - handle odd free form text that contains 'Yorkshire' and 'ALL' - will not be valid for future data
+region_count['Yorkshire and The Humber'] = region_count['Yorkshire and The Humber'] + 1
+total_reporting_region = total_reporting_region + 1
 
+for region in REGIONS:
+    region_count[region] = region_count[region] + 2
+total_reporting_region = total_reporting_region + 2
+
+
+max_region = max(region_count.values())
 with open('src\\report\\september-2022\\_data\\attendees.yml','w') as f:
     yaml.safe_dump(region_count,f)
 
@@ -27,7 +39,12 @@ with open('src\\report\\september-2022\\_data.yml','r') as r:
     info = yaml.safe_load(r)
 
 info['statistics']['total_returns'] = total_reporting
+info['statistics']['total_returns_region'] = total_reporting_region
 info['statistics']['max_attendees'] = max_region
 
 with open('src\\report\\september-2022\\_data.yml','w') as w:
     yaml.safe_dump(info,w)
+
+print(max_region)
+print(total_reporting_region)
+print(region_count)
